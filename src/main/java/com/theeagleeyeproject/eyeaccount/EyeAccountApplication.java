2a7cc72fc5878ec3f2d6522d0557944abb126ab6
jmart <jmartinez5120@gmail.com>
2023-10-22 21:26:28 -0400

    Initial commit of the repository. The app is running as expected.

---
+++ src/main/java/com/theeagleeyeproject/eyeaccount/EyeAccountApplication.java
@@ -0,0 +1,13 @@
+package com.theeagleeyeproject.eyeaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theeagleeyeproject/eyeaccount/EyeAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theeagleeyeproject.eyeaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
