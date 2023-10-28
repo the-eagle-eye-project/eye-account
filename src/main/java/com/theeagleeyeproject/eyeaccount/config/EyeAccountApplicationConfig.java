cf63230151dab4f9e70c660a00d9e7f8ff5fb3a2
johnmartinez <jmartinez5120@gmail.com>
2023-10-27 22:43:57 -0400

    The application is now saving into MongoDB.

--- src/main/java/com/theeagleeyeproject/eyeaccount/config/EyeAccountApplicationConfig.java
+++ src/main/java/com/theeagleeyeproject/eyeaccount/config/EyeAccountApplicationConfig.java
@@ -3,9 +3,11 @@ package com.theeagleeyeproject.eyeaccount.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import(ApplicationObjectMapper.class)
 @ComponentScan(basePackages = "com.theeagleeyeproject.eyeaccount")
+@PropertySource("classpath:application-${EAGLE-ENV}.properties")
 public class EyeAccountApplicationConfig {
 }
